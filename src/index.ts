eddda3afc031485aa7ccc1b27c761c1df3a7d5d3
ItzNesbro Dev <asnesbeer3@gmail.com>
2023-08-18 13:46:54 +0000

    feat: initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-export const foo = 'oo'
+#!/usr/bin/env node
+
+import readline from 'readline';
+import execSh from 'exec-sh';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+(async () => {
+  const choices = ['feat', 'fix', 'refactor', 'docs', 'style', 'test', 'build', 'ci'];
+
+  rl.question('Select a commit type:\n' + choices.map((c, i) => `${i + 1}. ${c}`).join('\n') + '\n', async (choiceIndex) => {
+    const choice = choices[Number(choiceIndex) - 1];
+
+    rl.question('Enter the commit message:\n', async (message) => {
+      const commitMessage = `${choice}: ${message}`;
+
+      execSh(`git commit -m "${commitMessage}"`, {}, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+
+      rl.close();
+    });
+  });
+})();
+
