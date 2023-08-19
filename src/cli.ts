56cc755997d0f4fc5e40c2a252702aca76bf00b3
ItzNesbroDev <asnesbeer3@gmail.com>
2023-08-19 10:11:38 +0530

    fix: change package.json version

--- src/cli.ts
+++ src/cli.ts
@@ -1,30 +1,43 @@
 #!/usr/bin/env node
 
-import readline from 'readline';
 import execSh from 'exec-sh';
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+import { Select, prompt as Inputs } from 'enquirer';
 
 (async () => {
   const choices = ['feat', 'fix', 'refactor', 'docs', 'style', 'test', 'build', 'ci'];
+  let selected;
+  let message;
 
-  rl.question('Select a commit type:\n' + choices.map((c, i) => `${i + 1}. ${c}`).join('\n') + '\n', async (choiceIndex) => {
-    const choice = choices[Number(choiceIndex) - 1];
+  const prompt = new Select({
+    name: 'type',
+    message: 'Select your commit type.',
+    multiple: false,
+    choices: choices
+  })
 
-    rl.question('Enter the commit message:\n', async (message) => {
-      const commitMessage = `${choice}: ${message}`;
+  prompt.run()
+    .then((answer: any) => {
+      selected = answer
+      runMessage()
+    })
+    .catch(console.error);
 
-      execSh(`git commit -m "${commitMessage}"`, {}, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-
-      rl.close();
+  async function runMessage() {
+    const messagePrompt = await Inputs({
+      type: 'input',
+      name: 'commitMessage',
+      message: 'Enter the commit message:',
     });
+
+    message = messagePrompt;
+  }
+
+  const commitMessage = `${selected}: ${message}`;
+
+  execSh(`git commit -m "${commitMessage}"`, {}, (err) => {
+    if (err) {
+      console.log(err);
+    }
   });
 })();
 
